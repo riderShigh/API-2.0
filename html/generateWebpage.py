b86df0b211407025fc215fcb7a661bd506edad3c
riderShigh <eltoncyho@gmail.com>
2015-08-31 22:34:43 -0700

    webpage related data p1

---
+++ html/generateWebpage.py
@@ -0,0 +1,27 @@
+import sys
+import urllib, json, os
+
+fBaseHtml = open('main.html');
+baseText = fBaseHtml.read();
+
+mageList = json.loads(open('mages.json').read());
+
+for key,value in mageList.iteritems():
+	#writeText = baseText.replace('Katarina',value);
+	champName = value.replace(" ","");
+	champName = champName.replace("'","");
+	champName = champName.lower();
+	champName = champName[0].upper() + champName[1:];
+
+	if champName == 'Fiddlesticks': champName = 'FiddleSticks';
+	if champName == 'Kogmaw': champName = 'KogMaw';
+	if champName == 'Twistedfate': champName = 'TwistedFate';
+	print champName;
+
+	writeText = baseText.replace('Katarina',champName);
+	writeText = writeText.replace('/1/champStats.json','/'+key+'/champStats.json');
+	writeText = writeText.replace('/tallied/1.json','/tallied/'+key+'.json');
+
+	fGenHtml = open('./'+champName+'.html','w');
+	fGenHtml.write(writeText);
+	fGenHtml.close();
