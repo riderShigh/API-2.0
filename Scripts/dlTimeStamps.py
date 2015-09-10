7a2f02b52aa144bd2bd583a1aa99c9e5310a5b69
riderShigh <eltoncyho@gmail.com>
2015-09-09 18:05:20 -0700

    comment update

--- Scripts/dlTimeStamps.py
+++ Scripts/dlTimeStamps.py
@@ -1,3 +1,11 @@
+# ************************************************
+# 	dlTimeStamps.py
+#	Download time stamps of games being played
+#	by champion mains
+#	
+#	Usage: python dlTimeStamps.py [champion]
+# ************************************************
+
 import sys
 import urllib, json, os, math
 import operator
@@ -9,28 +17,36 @@ import plotly.plotly as py
 from plotly.graph_objs import *
 py.sign_in('pa1087','5lnuuhu8xi');
 
+# Initialize patches, champions, regions, and time stamp objects
 patches = ['5.11','5.14'];
 thisChamp = sys.argv[1];
 region = ['BR','EUNE','EUW','KR','LAN','LAS','NA','OCE','RU','TR'];
 timeStamps = [];
 saveTimeStamps = {};
 
+# Load mains' summoner Id's and match Id's
 fGoodPlayerList = open('./champGoodMains.json');
 goodPlayerList = json.loads(fGoodPlayerList.read());
 
 fGoodPlayerMatchList = open('./champGoodMainMatches.json');
 goodPlayerMatchList = json.loads(fGoodPlayerMatchList.read());
 
+# Loop through regions
 for thisRegion in region:
 	saveTimeStamps[thisRegion] = {};
 	print 'on region ' + thisRegion;
+
+	# Loop through patches
 	for thisPatch in patches:
 		print 'on patch ' + str(thisPatch);
 		saveTimeStamps[thisRegion][thisPatch] = [];
+
+		# Loop through previously identified mains
 		for thisSummoner in goodPlayerList[thisPatch][thisRegion][thisChamp]:
 			timeStamps = [];
 			print 'summoner ' + str(thisSummoner);
-			#fMatchData = './' + thisPatch + '/RANKED_SOLO/' + thisRegion + '/' str(thisMatch) + '.json';
+			
+			# Download match list of this summoner where the desired champion is played
 			try:
 				fMatchList = dlURLData_matchList(thisRegion.lower(),str(thisSummoner),str(thisChamp),'RANKED_SOLO_5x5');
 				matchList = json.loads(fMatchList);
@@ -42,11 +58,14 @@ for thisRegion in region:
 			except:
 				print 'no total games';
 				continue;
+
+			# Record all time stamps
 			for i in xrange(0,int(matchList['totalGames'])):
 				timeStamps.append(matchList['matches'][i]['timestamp']);
 			saveTimeStamps[thisRegion][thisPatch].append({str(thisSummoner):timeStamps})
 
 
+# Save time stamps to file
 try:
 	os.makedirs('./pickDropStats/'+ str(thisChamp) + '/');
 except OSError as exception:
--- Scripts/filterPatchDates.py
+++ Scripts/filterPatchDates.py
@@ -1,3 +1,11 @@
+# ************************************************
+# 	filterPatchDates.py
+#	Filter out matches not played on patches
+#	5.10 to 5.16
+#	
+#	Usage: python filterPatchDates.py [champion]
+# ************************************************
+
 import sys
 import urllib, json, os, math
 import operator
@@ -7,24 +15,32 @@ from processing import *
 from download import *
 
 thisChamp = str(sys.argv[1]);
-#thisChamp = str(3);
+
+# Patch start times in epoch
 patch510epoch = 1432810800000;
 patch513epoch = 1436438400000;
 patch516epoch = 1440068400000;
 
+# Load time stamps for matches played by mains
 fTimeStamps = open('./pickDropStats/' + str(thisChamp) + '.json');
 timeStamps = json.loads(fTimeStamps.read());
 
 playCounts = {};
 summonerCount = 0;
-x = [];
-y = [];
 
+# Loop through regions
 for region,thisRegion in timeStamps.iteritems():
+
+	# Loop through patches
 	for patch,thisPatch in thisRegion.iteritems():
+
+		# Loop through summoners
 		for summoners in thisPatch:
-		#for summoner,thisSummoner in thisPatch.iteritems():
+
 			playCounts[summonerCount] = [0,0];
+
+			# Check which patch the match is played on
+			# Count increments when either match is in 5.10-5.13 or 5.14-5.16
 			for thisSummoner,matchTimeList in summoners.iteritems():
 				for matchTime in matchTimeList:
 					if matchTime > patch510epoch and matchTime < patch513epoch:
@@ -32,27 +48,13 @@ for region,thisRegion in timeStamps.iteritems():
 					if matchTime > patch513epoch and matchTime < patch516epoch:
 						playCounts[summonerCount][1] += 1;
 			summonerCount += 1;
-			#x.append(playCounts[str(summonerCount-1)][0]);
-			#y.append(playCounts[str(summonerCount-1)][1]);
 
+# Save filtered data to file
 try:
 	os.makedirs('./pickDropStats/filtered/');
 except OSError as exception:
 	if exception.errno != errno.EEXIST:
 		raise
-
-#analyzedData = tuple(analyzedData);
-#trace0 = Scatter(x=x,y=y,mode='markers',name='Data');
-#traceData = Data([trace0]);
-
-#layout = Layout(
-#	title= '',
-#	xaxis = XAxis(title='pre patch'),
-#	yaxis = YAxis(title='post patch')
-#	);
-#fig = Figure(data=traceData,layout=layout);
-
-#plot_url = py.plot(fig);
 
 fWriteFiltered = open('./pickDropStats/filtered/' + str(thisChamp) + '.json','w');
 json.dump(playCounts,fWriteFiltered);
